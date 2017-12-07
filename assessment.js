4776d604babda184488d8f46324f8359d83413cc
Justin DeMarco <justindemarco@Justins-MacBook-Pro.local>
2017-12-07 16:32:32 -0700

    assessment finished

--- assessment.js
+++ assessment.js
@@ -1,10 +1,12 @@
 
 // #1 Create a variable called hello and assign it the string 'goodbye'
 
-
+var hello = 'goodbye'
 
 // #2 Use the variable iLove to create a new variable called iLoveCode that is assigned the string "I love code"
 var iLove = 'I love';
+var iLoveCode = `${iLove} code`
+console.log(iLoveCode)
 
 // #3  Make an object called bob and give it the following properties
 // bob has a height of 6ft (string)
@@ -13,6 +15,16 @@ var iLove = 'I love';
 // bob is not presidentOfTheUnitedStates (boolean)
 // bob likes apples, bananas, and cherries (array of strings)
 
+var bob = {
+	height: '6ft',
+	age: 24,
+	hair: {
+		style: 'spikey',
+		color: 'brown'
+	},
+	presidentOfTheUnitedStates: false,
+	likes: ['apples','bananas','cherries']
+}
 
 // #4 Change my shirt color to pink using dot notation
 var myShirt = {
@@ -20,16 +32,26 @@ var myShirt = {
 	color: 'red'
 };
 
+myShirt.color = 'pink'
+console.log(myShirt.color)
+
 // Change my shirt type to spandex using square bracket notation
 var myOtherShirt = {
 	type: 'polo',
 	color: 'red'
 };
+myOtherShirt['type'] = 'spandex'
+console.log(myOtherShirt.type)
 
 // #5 Create an object that tracks a count of animals in a zoo.  Call it 'zoo'
 // The key should be the animal name(string) and the value should be how many there are.
 // Our zoo has 8 monkeys, 4 giraffes and 2 elephants
 
+var zoo = {
+	'monkeys': 8,
+	'giraffes': 4,
+	'elephants': 2
+};
 
 // #6 Loop through this object and change all keys that start with the letter s to have a value of 's'
 
@@ -41,47 +63,118 @@ var snake = {
 	runs: 'legless'
 };
 
-//#7 Create an array of strings that are the 7 primary colors in the rainbow - red, orange, yellow, green, blue, indigo, violet (lower-case). Call your array rainbowColors
+function changeVal (obj){
+	var snakeProp = [];
+	snakeProp.push(obj)
+	console.log(snakeProp)
+	for(var i = 0; i<snakeProp.length;i++){
+		var value = snakeProp[i]
+		console.log(value)
+		for(var i=0;i<value.length;i++){
+			var key = value[i]
+			console.log(key)
+			
+		}
+	}
+	console.log(obj);
+}
 
+
+changeVal(snake);
+//#7 Create an array of strings that are the 7 primary colors in the rainbow - red, orange, yellow, green, blue, indigo, violet (lower-case). Call your array rainbowColors
+var rainbowColors = ['red','orange','yellow','green','blue','indigo','violet']
 // #8 Using this array do the following
 var heroes = ['superman', 'batman', 'flash'];
 // add 'wonderwoman' to the end
-
+heroes.push('wonderwoman')
+console.log(heroes)
 // remove 'superman' and store him in a variable called firstHero
-
+var firstHero = heroes.splice(heroes[0],1)
+console.log(firstHero)
 // add 'spongebob' to the start of the array
-
+heroes.splice(heroes[0],0,'spongebob')
+console.log(heroes)
 // remove 'flash' from the array and store him in a variable called secondHero
-
+var secondHero = heroes.splice(2,1);
+console.log(secondHero)
+console.log(heroes)
 // leave batman in the array but put a copy of him on a variable called thirdHero
-
+var thirdHero = heroes.slice(1,2)
+console.log(thirdHero)
+console.log(heroes)
 
 
 
 // #9 Write a function called addItem that takes in an array and an item, adds the item to
 // the array, and returns the array with the added item.
-
+function addItem (arr,item){
+	arr.push(item);
+	return arr;
+}
+console.log(addItem([3,4,3,2,1],'happy'))
 
 // #10 Write a function called removeItem that takes in an array of strings, and a string.
-
 // Removes all instances of that string from the array. And return the modified array.
 // The order of the array should not be changed
-
+function removeItem(arr,string){
+	var arr1=[]
+	for(var i = 0;i<arr.length;i++){
+		if(arr[i]!==string){
+			console.log(string)
+			arr1.push(arr[i])
+		}
+	}
+	return arr1;
+}
+console.log(removeItem(['h','e','l','l','o'],'l'))
 // #11 Write a function  called doubleTheFun that takes 1 parameter. It should double numbers, and
 // repeats strings. example 4->8, 2.5->5, 'Awesome'->'AwesomeAwesome'
 
-
-
+function doubleTheFun(par){
+	if(par===){
+		Number(par)
+		return par*2
+	}else{
+		return par*2
+	}
+}
+console.log(doubleTheFun('fun'))
 // #12 Write function getValueOfProperty that takes in an object, and the name of a property on the object
 // return the value from the object that corresponds to the property
-
-
+function getValueOfProperty(obj,Name){
+	return obj[Name];
+}
+var obj = {
+	justin: 'j',
+	robert: 'r',
+	dan: 'd'
+}
+console.log(obj.justin)
+console.log(getValueOfProperty(obj,'justin'))
 // #13 Write a function called makeChatMessage that takes in a message and author as parameters
 // and returns an object with a message, author, and timestamp, that is
 // the current time as a Date object
+var makeChatMessage = (message,author) => {
+	
 
+}
 // #14 Create a function called coderTest that takes in an object that is a person. It looks to see if the person’s name is Jeremy and then changes the person object to have a property called lovesCode with a value of 10.  If their name is Brack set lovesCode to 0.  otherwise set lovesCode to 5.
-
+var coderTest = (obj) => {
+	var code=[]
+	code.push(obj)
+	for(var i =0;i<code.length;i++){
+		var val = code.name[i]
+		console.log(val)
+		if(val==="Jeremy"){
+			obj.lovesCode = 10;
+		}
+		if(val==='Brack'){
+			obj.lovesCode = 0;
+		}else{
+			obj.lovesCode=5;
+		}
+	}
+} 
 
 // #15 Create a function called outside that takes in a temperature (number), a humidity(number), and a cloudiness(number), in that order. Using the following to return the correct values
 /*
